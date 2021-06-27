99c7133bf7a9b518e9aef4522a05d1ed89ddee2f
Josemarepetto <48872679+Josemarepetto@users.noreply.github.com>
2021-06-26 23:49:10 -0300

    1

--- notificaciones.php
+++ notificaciones.php
@@ -1,7 +1,5 @@
 <?php
 
-    require './vendor/autoload.php';
-    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
     class webhook{
 
       public function __construct(){
@@ -20,7 +18,7 @@
                 echo $date_created;
                 //Grabo en archivo
                 file_put_contents('inputs.log',$date_created . PHP_EOL, FILE_APPEND);
-                echo '<script>alert("hola");</script>';
+                echo '<script>alert(".$date_created.");</script>';
             }
         }
         
